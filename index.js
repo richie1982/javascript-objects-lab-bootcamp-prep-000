826954f287488e848c2dbab62999b114817c787d
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-06 13:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = { eggs: 3 };
 function updateObjectWithKeyAndValue(recipes, key, value){
   return Object.assign({}, recipes, { [key]: value })
 }
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
+}
