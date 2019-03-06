72c9e1233e0612727f70cdcb543f32245f34a233
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-06 13:45:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function updateObjectWithKeyAndValue(recipes, key, value){
 }
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   recipes[key] = value;
-  return recipes
+  return recipes;
 }
+function 
